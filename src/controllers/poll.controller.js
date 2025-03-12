f01a2667dbafff640ca1c95f54d58fa155b505b4
mdalaminfaraji <alaminice1617@gmail.com>
2025-03-12 17:52:02 +0600

    off already voted on this poll

--- src/controllers/poll.controller.js
+++ src/controllers/poll.controller.js
@@ -1,7 +1,7 @@
-const Poll = require('../models/poll.model');
-const { createError } = require('../utils/error.utils');
-const { validatePoll, validateVote } = require('../utils/validation.utils');
-const { hashIP } = require('../utils/privacy.utils');
+const Poll = require("../models/poll.model");
+const { createError } = require("../utils/error.utils");
+const { validatePoll, validateVote } = require("../utils/validation.utils");
+const { hashIP } = require("../utils/privacy.utils");
 
 // Create a new poll
 exports.createPoll = async (req, res, next) => {
@@ -16,19 +16,19 @@ exports.createPoll = async (req, res, next) => {
     // Calculate expiration date based on user selection
     let expirationMs;
     switch (expiresIn) {
-      case '1hour':
+      case "1hour":
         expirationMs = 60 * 60 * 1000; // 1 hour
         break;
-      case '12hours':
+      case "12hours":
         expirationMs = 12 * 60 * 60 * 1000; // 12 hours
         break;
-      case '24hours':
+      case "24hours":
       default:
         expirationMs = 24 * 60 * 60 * 1000; // 24 hours (default)
     }
 
     // Format options array
-    const formattedOptions = options.map(option => ({ text: option }));
+    const formattedOptions = options.map((option) => ({ text: option }));
 
     // Create new poll
     const poll = new Poll({
@@ -49,8 +49,8 @@ exports.createPoll = async (req, res, next) => {
         options: savedPoll.options,
         expiresAt: savedPoll.expiresAt,
         hideResults: savedPoll.hideResults,
-        isPrivate: savedPoll.isPrivate
-      }
+        isPrivate: savedPoll.isPrivate,
+      },
     });
   } catch (err) {
     next(err);
@@ -61,16 +61,18 @@ exports.createPoll = async (req, res, next) => {
 exports.getPollById = async (req, res, next) => {
   try {
     const { pollId } = req.params;
-    
+
     const poll = await Poll.findOne({ pollId });
-    
+
     if (!poll) {
-      return next(createError(404, 'Poll not found'));
+      return next(createError(404, "Poll not found"));
     }
 
     // Check if poll has expired
     if (Date.now() > new Date(poll.expiresAt).getTime()) {
-      return next(createError(410, 'This poll has expired and is no longer available'));
+      return next(
+        createError(410, "This poll has expired and is no longer available")
+      );
     }
 
     // If hideResults is true and poll is still active, don't send vote counts
@@ -79,14 +81,14 @@ exports.getPollById = async (req, res, next) => {
       data: {
         pollId: poll.pollId,
         title: poll.title,
-        options: poll.hideResults ? 
-          poll.options.map(opt => ({ _id: opt._id, text: opt.text })) : 
-          poll.options,
+        options: poll.hideResults
+          ? poll.options.map((opt) => ({ _id: opt._id, text: opt.text }))
+          : poll.options,
         expiresAt: poll.expiresAt,
         hideResults: poll.hideResults,
         isPrivate: poll.isPrivate,
-        reactions: poll.reactions
-      }
+        reactions: poll.reactions,
+      },
     };
 
     res.json(response);
@@ -100,7 +102,7 @@ exports.votePoll = async (req, res, next) => {
   try {
     const { pollId } = req.params;
     const { optionId } = req.body;
-    
+
     // Validate request
     const { error } = validateVote(req.body);
     if (error) {
@@ -108,15 +110,20 @@ exports.votePoll = async (req, res, next) => {
     }
 
     // Find the poll
-    const poll = await Poll.findOne({ pollId }).select('+voterIps');
-    
+    const poll = await Poll.findOne({ pollId }).select("+voterIps");
+
     if (!poll) {
-      return next(createError(404, 'Poll not found'));
+      return next(createError(404, "Poll not found"));
     }
 
     // Check if poll has expired
     if (Date.now() > new Date(poll.expiresAt).getTime()) {
-      return next(createError(410, 'This poll has expired and votes are no longer accepted'));
+      return next(
+        createError(
+          410,
+          "This poll has expired and votes are no longer accepted"
+        )
+      );
     }
 
     // Get user IP and hash it for privacy
@@ -124,15 +131,17 @@ exports.votePoll = async (req, res, next) => {
     const hashedIp = hashIP(ip);
 
     // Check if user has already voted
-    if (poll.voterIps.includes(hashedIp)) {
-      return next(createError(403, 'You have already voted on this poll'));
-    }
+    // if (poll.voterIps.includes(hashedIp)) {
+    //   return next(createError(403, 'You have already voted on this poll'));
+    // }
 
     // Find the option to vote for
-    const optionIndex = poll.options.findIndex(opt => opt._id.toString() === optionId);
-    
+    const optionIndex = poll.options.findIndex(
+      (opt) => opt._id.toString() === optionId
+    );
+
     if (optionIndex === -1) {
-      return next(createError(404, 'Option not found in this poll'));
+      return next(createError(404, "Option not found in this poll"));
     }
 
     // Increment vote count and add user IP to the list
@@ -147,12 +156,12 @@ exports.votePoll = async (req, res, next) => {
       data: {
         pollId: poll.pollId,
         title: poll.title,
-        options: poll.hideResults ? 
-          poll.options.map(opt => ({ _id: opt._id, text: opt.text })) : 
-          poll.options,
+        options: poll.hideResults
+          ? poll.options.map((opt) => ({ _id: opt._id, text: opt.text }))
+          : poll.options,
         expiresAt: poll.expiresAt,
-        message: 'Vote recorded successfully'
-      }
+        message: "Vote recorded successfully",
+      },
     };
 
     res.json(response);
@@ -166,24 +175,31 @@ exports.addReaction = async (req, res, next) => {
   try {
     const { pollId } = req.params;
     const { reactionType } = req.body;
-    
-    if (!reactionType || !['trending', 'like'].includes(reactionType)) {
-      return next(createError(400, 'Invalid reaction type. Must be "trending" or "like"'));
+
+    if (!reactionType || !["trending", "like"].includes(reactionType)) {
+      return next(
+        createError(400, 'Invalid reaction type. Must be "trending" or "like"')
+      );
     }
 
     const poll = await Poll.findOne({ pollId });
-    
+
     if (!poll) {
-      return next(createError(404, 'Poll not found'));
+      return next(createError(404, "Poll not found"));
     }
 
     // Check if poll has expired
     if (Date.now() > new Date(poll.expiresAt).getTime()) {
-      return next(createError(410, 'This poll has expired and reactions are no longer accepted'));
+      return next(
+        createError(
+          410,
+          "This poll has expired and reactions are no longer accepted"
+        )
+      );
     }
 
     // Increment the appropriate reaction counter
-    if (reactionType === 'trending') {
+    if (reactionType === "trending") {
       poll.reactions.trending += 1;
     } else {
       poll.reactions.likes += 1;
@@ -196,8 +212,8 @@ exports.addReaction = async (req, res, next) => {
       data: {
         pollId: poll.pollId,
         reactions: poll.reactions,
-        message: 'Reaction added successfully'
-      }
+        message: "Reaction added successfully",
+      },
     });
   } catch (err) {
     next(err);
@@ -208,16 +224,18 @@ exports.addReaction = async (req, res, next) => {
 exports.getPollResults = async (req, res, next) => {
   try {
     const { pollId } = req.params;
-    
+
     const poll = await Poll.findOne({ pollId });
-    
+
     if (!poll) {
-      return next(createError(404, 'Poll not found'));
+      return next(createError(404, "Poll not found"));
     }
 
     // Check if poll has expired or if hideResults is false
     if (poll.hideResults && Date.now() < new Date(poll.expiresAt).getTime()) {
-      return next(createError(403, 'Results for this poll are hidden until it expires'));
+      return next(
+        createError(403, "Results for this poll are hidden until it expires")
+      );
     }
 
     res.json({
@@ -228,8 +246,8 @@ exports.getPollResults = async (req, res, next) => {
         options: poll.options,
         expiresAt: poll.expiresAt,
         reactions: poll.reactions,
-        isExpired: Date.now() > new Date(poll.expiresAt).getTime()
-      }
+        isExpired: Date.now() > new Date(poll.expiresAt).getTime(),
+      },
     });
   } catch (err) {
     next(err);
